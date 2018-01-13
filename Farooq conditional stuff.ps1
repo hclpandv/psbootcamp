529724c8b5098d23d91b6a27e1a640e395e25aa5
Vikas Pandey <vikaspa6@gmail.com>
2018-01-13 15:43:37 +0000

    Add files via upload

---
+++ Farooq conditional stuff.ps1
@@ -0,0 +1,33 @@
+﻿$age = Read-Host "Please enter your age?"
+$gender = Read-Host "Please Enter your Gender i.e. M/F"
+
+if($gender -eq "F")
+{
+
+        if ($age -gt 18)
+        {
+            Write-Host "You are allowed to marry"
+        }
+
+        else
+        {
+            Write-Host "You are not allowed to marry"
+        }
+
+}
+else
+{
+
+
+        if ($age -gt 21)
+        {
+            Write-Host "You are allowed to marry"
+        }
+
+        else
+        {
+            Write-Host "You are not allowed to marry"
+        }
+
+
+}
