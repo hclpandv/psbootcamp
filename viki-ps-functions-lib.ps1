ac7f8658cac38acb42d1574f85f6cfd5aa1fa1b0
Vikas Pandey <vikaspa6@gmail.com>
2018-03-03 13:00:10 +0530

    Update viki-ps-functions-lib.ps1

--- viki-ps-functions-lib.ps1
+++ viki-ps-functions-lib.ps1
@@ -1,8 +1,8 @@
-RED($msg){write-host $msg -ForegroundColor Red}
-BLUE($msg){write-host $msg -ForegroundColor Blue}
-CYAN($msg){write-host $msg -ForegroundColor Cyan}
-DARKBLUE($msg){write-host $msg -ForegroundColor DarkBlue}
-DARKRED($msg){write-host $msg -ForegroundColor DarkRed}
-DARKYELLOW($msg){write-host $msg -ForegroundColor DarkYellow}
-WHITE($msg){write-host $msg -ForegroundColor White}
-YELLOW($msg){write-host $msg -ForegroundColor Yellow}
+function RED($msg){write-host $msg -ForegroundColor Red}
+function BLUE($msg){write-host $msg -ForegroundColor Blue}
+function CYAN($msg){write-host $msg -ForegroundColor Cyan}
+function DARKBLUE($msg){write-host $msg -ForegroundColor DarkBlue}
+function DARKRED($msg){write-host $msg -ForegroundColor DarkRed}
+function DARKYELLOW($msg){write-host $msg -ForegroundColor DarkYellow}
+function WHITE($msg){write-host $msg -ForegroundColor White}
+function YELLOW($msg){write-host $msg -ForegroundColor Yellow}
